451df60b5b711c54a5e97d4ea3a1f27f423cc452
Devendra <devdangare88@gmail.com0>
2024-03-16 11:51:29 +0530

    folder addded and profile page start

---
+++ src/Pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from "../Styles/ProfilePage.module.css"
+export default function ProfilePage() {
+  return (
+    <>
+        <h1>Profile Page</h1>
+    </>
+  )
+}
